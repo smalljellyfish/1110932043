57ae722a2414f2750aa06291e957c639461379a4
bigjellyfish <90674333+smalljellyfish@users.noreply.github.com>
2022-01-07 23:47:07 +0800

    Add files via upload

---
+++ 首頁.js
@@ -0,0 +1,37 @@
+//確認框//
+function logout_1(){ 
+if (confirm("有毒你還要去？")){ 
+  window.location.href="王子神谷鬆餅店.html" 
+} 
+}
+
+function logout_2(){ 
+if (confirm("有毒你還要去？")){ 
+  window.location.href="豐仁冰.html" 
+} 
+}
+
+function logout_3(){ 
+if (confirm("有毒你還要去？")){ 
+  window.location.href="丸勝咖哩.html" 
+} 
+}
+
+function logout_4(){ 
+if (confirm("有毒你還要去？")){ 
+  window.location.href="星空Stellato.html" 
+} 
+}
+
+function logout_5(){ 
+if (confirm("有毒你還要去？")){ 
+  window.location.href="川牛.html" 
+} 
+}
+
+function logout_6(){ 
+if (confirm("有毒你還要去？")){ 
+  window.location.href="二口美食鐵板麵.html" 
+} 
+}
+
